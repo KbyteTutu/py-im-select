599dc17de3625960da6188fe2c7b8b58e3fd5d98
tukechao <tukechao@gmail.com>
2022-06-21 01:34:11 +0800

    ====feat:功能更新====

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # py-im-select
 vim im-select plugin python implement
+
+It provides the command line program for VSCodeVim's autoSwitchIM function
+
+本工具就是自用配合搜狗输入法的。想了个招，先快速切换到英文再切回搜狗，重置了搜狗的输入状态，然后再shift一下起到切英文的效果。
---
+++ win/im-select.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @ 南无阿弥陀佛，不要有太多bug……
+# @ Author: tukechao
+# @ Date: 2022-06-21 00:12:30
+# @ LastEditors: tukechao
+# @ LastEditTime: 2022-06-21 01:33:03
+# @ FilePath: \Python\py-im-select\win\im-select.py
+# @ Description: vim 输入法切换工具(配合搜狗输入法)
+# @ 如有疑问请联系 tukechao@gmail.com
+
+import win32api
+import win32gui
+import win32con
+import sys
+from win32con import WM_INPUTLANGCHANGEREQUEST
+
+
+def get_current_im():
+    hwnd = win32gui.GetForegroundWindow()
+    if hwnd:
+        thread_id = win32api.GetWindowLong(hwnd, 0)
+        klid = win32api.GetKeyboardLayout(thread_id)
+        current_lid = klid & (2**16 - 1)
+        return current_lid
+    else:
+        return 0
+
+
+def switch_im_to_needed(lid):
+    lang = {"2052": 0x804, "1033": 0x409}
+
+    hwnd = win32gui.GetForegroundWindow()
+    if hwnd:
+        win32api.SendMessage(hwnd, WM_INPUTLANGCHANGEREQUEST, 0, eval(lid))
+    # if lid == hex(2052):
+    #     simulate_press_shift()
+
+
+# def simulate_press_shift():
+#     win32api.keybd_event(16, 0, win32con.KEYEVENTF_KEYUP, 0)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        lid = hex(int(sys.argv[1]))
+        switch_im_to_needed(lid)
+    else:
+        print(get_current_im())
